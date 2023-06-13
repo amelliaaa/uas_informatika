bdffb1b4492358904ebef6c8b09a6733e3677597
amelliaaa <125233235+amelliaaa@users.noreply.github.com>
2023-06-13 12:59:49 +0700

    uas informatika

---
+++ uas1.php
@@ -0,0 +1,56 @@
+
+<?php
+$kaloriPerMakanan = 2500;
+$kaloriPerHari = 0;
+$kaloriPerMinggu = 0;
+
+// Senin
+$senin = (3 * $kaloriPerMakanan);
+$kaloriPerMinggu = (3 * $kaloriPerMakanan);
+
+// Selasa
+$selasa= (2 * $kaloriPerMakanan);
+$kaloriPerMinggu = (2 * $kaloriPerMakanan);
+
+// Rabu
+$rabu = (2 * $kaloriPerMakanan);
+$kaloriPerMinggu = (2 * $kaloriPerMakanan);
+
+// Kamis
+$kamis = (3 * $kaloriPerMakanan);
+$kaloriPerMinggu = (3 * $kaloriPerMakanan);
+
+// Jumat
+$jumat = (3 * $kaloriPerMakanan);
+$kaloriPerMinggu = (3 * $kaloriPerMakanan);
+
+// Sabtu
+$sabtu = (2 * $kaloriPerMakanan);
+$kaloriPerMinggu = (2 * $kaloriPerMakanan);
+
+// Minggu
+$minggu = (1 * $kaloriPerMakanan);
+$kaloriPerMinggu = (1 * $kaloriPerMakanan);
+
+echo "Senin: " . $senin . "<br>";
+echo "Selasa: " . $selasa . "<br>";
+echo "Rabu: " . $rabu . "<br>";
+echo "Kamis: " . $kamis . "<br>";
+echo "Jumat: " . $jumat . "<br>";
+echo "Sabtu: " . $sabtu . "<br>";
+echo "Minggu: " . $minggu . "<br>";
+
+echo "Kalori per minggu " . $JumlahSeminggu = $senin + $selasa + $rabu + $kamis + $jumat + $sabtu + $minggu ;
+
+$senin1 = 5;
+$selasa1 = 6;
+$rabu1 = 8;
+$kamis1 = 8;
+$jumat1 = 5;
+$sabtu1 = 6;
+$minggu1 = 4;
+
+echo "jumlah istirahat perminggu " . $Jumlahistirahat = $senin1 + $selasa1 + $rabu1 + $kamis1 + $jumat1 + $sabtu1 + $minggu1 , "</br>";
+
+ echo "dari hasil perhitungan kalori Fu akan sembuh, karena kalori terpenenuhi dan jam istirahat sudah cukup";
+?>
